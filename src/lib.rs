12945c3f5994a4f69dc24d354cf3ffb01e822e6e
Abhinandh S <ugabhi@proton.me>
2024-11-08 18:22:03 +0530

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,21 @@
 use std::error::Error;
+use std::fs;
 use std::path::Path;
+
+use dirs::data_local_dir;
+
+pub fn remove_file(file: &Path) -> Result<(),Box<dyn Error>> {
+    let p = fs::exists(file)?;
+    let dest = if p {
+        let trash_dir = data_local_dir().expect("Failed to get local data directory").join("Trash/files").join(file);
+        println!("{}", trash_dir.display());
+        "ds".to_string()
+    } else {
+        return Ok(());
+    };
+    println!("{}", dest);
+    Ok(())
+}
 
 pub fn recursive_remove(dir: &Path) -> Result<(),Box<dyn Error>> {
     println!("{}", &dir.display());
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
-use gr::recursive_remove;
+use gr::{recursive_remove, remove_file};
 
 #[derive(Parser)]
-#[command(version, about, long_about = "By default, gr does not remove directories.  Use the --recursive (-r) option to remove each listed directory, too, along with all of its contents.")]
+#[command(version, about, long_about = "By default, gr does not remove directories. Use the --recursive (-r) option to remove each listed directory, too, along with all of its contents.")]
 struct Cli {
     /// Remove files
     file: Option<PathBuf>,
@@ -30,6 +30,7 @@ fn main() {
 
     if let Some(file) = cli.file.as_deref() {
         println!("Value for name: {}", file.display());
+        remove_file(file).unwrap();
     }
 
     if let Some(path) = cli.recursive.as_deref() {
@@ -46,5 +47,32 @@ fn main() {
             }
         }
         None => {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use std::path::Path;
+
+    use super::*;
+
+    #[test]
+    fn single_file_test() {
+        fs::write("tmp_file.txt", "test contents").unwrap();
+        assert!(fs::exists("tmp_file.txt").is_ok());
+        let p = Path::new("tmp_file.txt");
+        remove_file(p).unwrap();
+        assert!(!fs::exists("tmp_file.txt").expect("Can't check existence of file tmp_file.txt"));
+    }
+
+    #[test]
+    fn recursive_remove_test() {
+        fs::create_dir_all("some/dir/for/testing").unwrap();
+        fs::write("some/dir/for/testing/tmp_file.txt", "test contents").unwrap();
+        let p = Path::new("some/dir/for/testing/tmp_file.txt");
+        assert!(fs::exists("some/dir/for/testing/tmp_file.txt").is_ok());
+        recursive_remove(p).unwrap();
+        assert!(!fs::exists("some/dir/for/testing/tmp_file.txt").expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
     }
 }

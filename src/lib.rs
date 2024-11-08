5825d150fee2e7d6cb4ce40459af90b5289af1eb
Abhinandh S <ugabhi@proton.me>
2024-11-08 22:04:38 +0530

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,60 @@
+use std::env::current_dir;
 use std::error::Error;
-use std::path;
+use std::{fs, path};
 use std::path::Path;
 
+use chrono::Local;
 use dirs::data_local_dir;
 
 pub fn remove_file(file: &Path) -> Result<(),Box<dyn Error>> {
     let p = path::Path::new(file).exists();
-    println!("File: {}", &file.display());
-    let dest = if p {
-        let trash_dir = data_local_dir().expect("Failed to get local data directory").join("Trash/files").join(file);
-        println!("{}", trash_dir.display());
-        "ds".to_string()
+    if p {
+        let trash_dir = data_local_dir().expect("Failed to get local data directory").join("Trash/files");
+        dbg!("{}", &trash_dir);
+        if trash_dir.join(file).exists() {
+            let c_time = Local::now();
+            let formatted_time = c_time.format("%Y-%m-%d_%H:%M:%S").to_string();
+            let stem_name = Path::new(&file).file_stem().expect("Err").to_str().unwrap();
+            let ext = Path::new(&file).extension().unwrap().to_str().unwrap();
+            let new_name = stem_name.to_string() + "." + &formatted_time + "." + ext;
+            let new = format!("{}",trash_dir.join(new_name).display());
+            println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
+            println!("target: {}", &new);
+            fs::rename(file, new)?;
+        } else {
+            println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
+            println!("target: {}", &trash_dir.join(file).display());
+            fs::rename(file, trash_dir.join(file))?;
+        }
     } else {
         eprintln!("rid: cannot remove '{}': No such file or directory", &file.display());
         return Ok(());
     };
-    println!("{}", dest);
     Ok(())
 }
 
 pub fn recursive_remove(dir: &Path) -> Result<(),Box<dyn Error>> {
-    println!("{}", &dir.display());
+    let p = path::Path::new(dir).exists();
+    if p {
+        let trash_dir = data_local_dir().expect("Failed to get local data directory").join("Trash/files").join(dir);
+        if trash_dir.exists() {
+            let c_time = Local::now();
+            let formatted_time = c_time.format("%Y-%m-%d_%H:%M:%S").to_string();
+            eprintln!("rid: {} already exists", &trash_dir.display());
+            let new_name = format!("{}.{}",&trash_dir.to_str().unwrap().to_string(), &formatted_time);
+            println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
+            println!("target: {}", &new_name);
+            fs::rename(dir, new_name)?;
+        } else {
+            // moves the file or dir to trash with same name. 
+            println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
+            println!("target: {}", &trash_dir.display());
+            fs::rename(dir, &trash_dir)?;
+        }
+        trash_dir.to_str().unwrap().to_string()
+    } else {
+        eprintln!("rid: cannot remove '{}': No such file or directory", &dir.display());
+        return Ok(());
+    };
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use clap::{Parser, Subcommand};
 use rid::{recursive_remove, remove_file};
 
 #[derive(Parser)]
-#[command(version, about, long_about = "By default, gr does not remove directories. Use the --recursive (-r) option to remove each listed directory, too, along with all of its contents.")]
+#[command(version, about, long_about = "By default, rid does not remove directories. Use the --recursive (-r) option to remove each listed directory, too, along with all of its contents.")]
 struct Cli {
     /// Remove files
     file: Option<PathBuf>,
@@ -29,12 +29,10 @@ fn main() {
     let cli = Cli::parse();
 
     if let Some(file) = cli.file.as_deref() {
-        println!("Value for name: {}", file.display());
         remove_file(file).unwrap();
     }
 
     if let Some(path) = cli.recursive.as_deref() {
-        println!("Value for config: {}", path.display());
         recursive_remove(path).unwrap();
     }
 
@@ -52,6 +50,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
+    use std::fs::{remove_dir_all, remove_file};
     use std::fs;
     use std::path::Path;
 
@@ -59,20 +58,28 @@ mod tests {
 
     #[test]
     fn single_file_test() {
-        fs::write("tmp_file.txt", "test contents").unwrap();
-        assert!(fs::exists("tmp_file.txt").is_ok());
-        let p = Path::new("tmp_file.txt");
+        let t_file: String = "temp_file.org".into(); 
+        let s =  Path::new(&t_file).exists();
+        if s {
+            remove_file(&t_file).unwrap();
+        }
+        assert!(!fs::exists(&t_file).expect("Can't check existence of file tmp_file.txt"));
+        fs::write(&t_file, "test contents").unwrap();
+        assert!(fs::exists(&t_file).is_ok());
+        let p = Path::new(&t_file);
         remove_file(p).unwrap();
-        assert!(!fs::exists("tmp_file.txt").expect("Can't check existence of file tmp_file.txt"));
+        assert!(!fs::exists(t_file).expect("Can't check existence of file tmp_file.txt"));
     }
 
     #[test]
     fn recursive_remove_test() {
+        let s =  Path::new("some").exists();
+        if s { remove_dir_all("some").unwrap(); }
         fs::create_dir_all("some/dir/for/testing").unwrap();
-        fs::write("some/dir/for/testing/tmp_file.txt", "test contents").unwrap();
-        let p = Path::new("some/dir/for/testing/tmp_file.txt");
-        assert!(fs::exists("some/dir/for/testing/tmp_file.txt").is_ok());
-        recursive_remove(p).unwrap();
-        assert!(!fs::exists("some/dir/for/testing/tmp_file.txt").expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        fs::write("some/test.txt", "some contents for testing").unwrap();
+        recursive_remove(Path::new("some")).unwrap();
+        assert!(!fs::exists("some").expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        assert!(!fs::exists("some/dir/for").expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
+        assert!(!fs::exists("some/dir/for/testing").expect("Can't check existence of file some/dir/for/testing/tmp_file.txt"));
     }
 }

f6674769d1dc7c7350c3dd9de2d9a45f889e1e67
Abhinandh S <ugabhi@proton.me>
2024-11-08 12:45:18 +0530

    Refactoring

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    unstable-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, unstable-nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [
+          (import rust-overlay)
+          (final: prev: {
+            unstable = import unstable-nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+        {
+        devShells.default = import ./nix/shell.nix { inherit pkgs; }; 
+      }
+    );
+}

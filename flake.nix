bc3e16d048ba8bee3e61aedd4030f6bbf5703592
Abhinandh S <ugabhi@proton.me>
2024-11-10 19:34:44 +0530

    Refactoring

--- flake.nix
+++ flake.nix
@@ -1,38 +1,20 @@
 {
-  description = "A devShell example";
+  description = "A very basic flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-    unstable-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, unstable-nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
-        overlays = [
-          (import rust-overlay)
-          (final: prev: {
-            unstable = import unstable-nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          })
-        ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-      in
-        {
-        devShells.default = import ./nix/shell.nix { inherit pkgs; };
-        packages = {
-          ${manifest.name} = pkgs.callPackage ./nix/default.nix { };
-          default = pkgs.callPackage ./nix/default.nix { };
-        };
-        homeManagerModules.rid = import ./nix/module.nix { inherit pkgs; };
-        homeManagerModules.default = self.homeManagerModules.rid;
-      }
-    );
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system}; 
+    manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+  in {
+    devShells.default = import ./nix/shell.nix {};
+
+    packages.${system} = {
+      ${manifest.name} = pkgs.callPackage ./nix/release/default.nix { };
+      default = self.packages.${system}.${manifest.name};
+    };
+  };
 }
--- justfile
+++ justfile
@@ -1,3 +1,6 @@
+remote-run:
+  nix run github:abhi-xyz/rid -- help
+
 release:
   git tag v0.1.2
   git push --tags

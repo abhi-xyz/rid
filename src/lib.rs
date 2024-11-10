10570cda01159e93877258f1a2572f9a6c1eaec8
Abhinandh S <ugabhi@proton.me>
2024-11-10 22:03:06 +0530

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -25,12 +25,12 @@ pub fn remove_file(file: Vec<PathBuf>) -> Result<(),Box<dyn Error>> {
                 let ext = Path::new(&i).extension().unwrap().to_str().unwrap();
                 let new_name = stem_name.to_string() + "." + &formatted_time + "." + ext;
                 let new = format!("{}",trash_dir.join(new_name).display());
-                println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
-                println!("target: {}", &new);
+                // println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
+                // println!("target: {}", &new);
                 fs::rename(i, &new)?;
             } else {
-                println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
-                println!("target: {}", &trash_dir.join(&i).display());
+                // println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
+                // println!("target: {}", &trash_dir.join(&i).display());
                 fs::rename(&i, trash_dir.join(&i))?;
             }
         } else {
@@ -40,7 +40,6 @@ pub fn remove_file(file: Vec<PathBuf>) -> Result<(),Box<dyn Error>> {
     }
     Ok(())
 }
-
 pub fn recursive_remove(dir: Vec<PathBuf>) -> Result<(),Box<dyn Error>> {
     for i in dir {
     let p = path::Path::new(&i).exists();
@@ -49,10 +48,12 @@ pub fn recursive_remove(dir: Vec<PathBuf>) -> Result<(),Box<dyn Error>> {
         if trash_dir.exists() {
             let c_time = Local::now();
             let formatted_time = c_time.format("%Y-%m-%d_%H:%M:%S").to_string();
-            eprintln!("rid: {} already exists", &trash_dir.display());
-            let new_name = format!("{}.{}",&trash_dir.to_str().unwrap().to_string(), &formatted_time);
-            println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
-            println!("target: {}", &new_name);
+            let file_name = Path::new(&trash_dir).file_name().expect("Failed to extract file names from Trash");
+            let new_trash_path = data_local_dir().expect("Failed to get local data directory").join("Trash/files").join(file_name);
+            let new_name = format!("{}.{}",&new_trash_path.display(), &formatted_time);
+            eprintln!("rid: {} already exists\nTrashed as {}", &trash_dir.display(), &new_name);
+            // println!("pwd: {}", current_dir().unwrap().to_str().unwrap());
+            // println!("target: {}", &new_name);
             fs::rename(i, new_name)?;
         } else {
             // moves the file or dir to trash with same name. 

8a883901670afab5ea301b08ec8b6a77a39aa797
Abhinandh S <ugabhi@proton.me>
2024-11-14 16:47:46 +0530

    Refactoring

--- tests/test.rs
+++ tests/test.rs
@@ -1,8 +1,11 @@
+use std::fs;
+
 #[cfg(test)]
 mod tests {
 
     #[test]
-    fn glob_test() {
-        println!("Tests");
+    fn github_test() {
+        fs::write("file_for_github.txt", "some contents for the files").expect("Cant create files for github");
+        fs::create_dir_all("some/dir/for/testing").expect("Cant create files for github");
     }
 }

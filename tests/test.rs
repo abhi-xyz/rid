77034fb1a435e7e5fbde29da05f5d5fe5b4dfa84
Abhinandh S <ugabhi@proton.me>
2024-11-14 20:01:41 +0530

    Refactoring

--- tests/test.rs
+++ tests/test.rs
@@ -9,25 +9,31 @@ mod tests {
 
     #[test]
     fn github_test() {
-        let v0 = PathBuf::from("temp_file_for_single_file_text01.txt");
-            let v1 = PathBuf::from("temp_file_for_single_file_text02.txt");
-            let v2 = PathBuf::from("temp_file_for_single_file_text03.txt");
-            let v3 = PathBuf::from("temp_file_for_single_file_text04.txt");
-            let single_files = vec![v0, v1, v2, v3];
-            for i in &single_files {
-                fs::write(i, "some contents for the files").expect("Cant create files");
-                assert!(fs::exists(i).expect("Can't check existence of file tmp_file.txt"));
-            }
         if trash_dir().exists() {
-            remove_files(single_files, false, true).expect("Test failed");
-            // fs::write("file_for_github.txt", "some contents for the files").expect("Cant create files for github");
-            // fs::create_dir_all("some/dir/for/testing").expect("Cant create files for github");
-
-            // remove_files("file_for_github.txt", false, true);
-            // remove_files("some", true, false);
+            panic!("trash dir exists!");
         } else {
-            create_dir_all("~/.local/share/Trash/files").expect("FAILED TO CREATE TRASH DIR FROM TEST");
-            remove_files(single_files, false, true).expect("Test failed");
+            panic!("trash dir doesn't exists");
         }
+        
+        //let v0 = PathBuf::from("temp_file_for_single_file_text01.txt");
+        //    let v1 = PathBuf::from("temp_file_for_single_file_text02.txt");
+        //    let v2 = PathBuf::from("temp_file_for_single_file_text03.txt");
+        //    let v3 = PathBuf::from("temp_file_for_single_file_text04.txt");
+        //    let single_files = vec![v0, v1, v2, v3];
+        //    for i in &single_files {
+        //        fs::write(i, "some contents for the files").expect("Cant create files");
+        //        assert!(fs::exists(i).expect("Can't check existence of file tmp_file.txt"));
+        //    }
+        //if trash_dir().exists() {
+        //    remove_files(single_files, false, true).expect("Test failed");
+        //    // fs::write("file_for_github.txt", "some contents for the files").expect("Cant create files for github");
+        //    // fs::create_dir_all("some/dir/for/testing").expect("Cant create files for github");
+        //
+        //    // remove_files("file_for_github.txt", false, true);
+        //    // remove_files("some", true, false);
+        //} else {
+        //    create_dir_all("~/.local/share/Trash/files").expect("FAILED TO CREATE TRASH DIR FROM TEST");
+        //    remove_files(single_files, false, true).expect("Test failed");
+        //}
     }
 }
